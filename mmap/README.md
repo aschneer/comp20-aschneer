bbe2639ff9f88f9e55cac70d3564ea6f71c53f30
Andrew_Schneer <aschneer738@gmail.com>
2015-03-11 06:23:33 -0400

    Finshed implementing Google Map interface and also datastore API stuff.  There is a problem with creating other markers besides my own.

---
+++ mmap/README.md
@@ -0,0 +1,8 @@
+Andrew Schneer
+Tufts University
+COMP 20 - Web Programming
+Ming Chow
+Assignment 02
+"The Real Marauder's Map"
+03/12/2015
+
--- mmap/js/mmap.js
+++ mmap/js/mmap.js
@@ -1,8 +1,9 @@
 // Global Variables:
+var myLogin = "KelleyAmmerman";
 var myLat = 0;
 var myLng = 0;
-var request = new XMLHttpRequest();
-var myLatLng = new google.maps.LatLng(myLat, myLng);
+var ajaxObj = new XMLHttpRequest();
+var myLatLng = new google.maps.LatLng(myLat,myLng);
 // Array containing initial options for the map.
 var mapOptions = {
 		zoom: 8,
@@ -10,7 +11,64 @@ var mapOptions = {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 var map;
+// Variables for my position marker.
+var myMarkerOptions;
 var myMarker;
+var myMarkerClickListener;
+var myInfoWindow;
+// Arrays to hold position marker
+// information for the rest of
+// the class.
+var classMarkerOptions;
+var classMarkers;
+var classMarkerClickListeners;
+var classInfoWindows;
+// Class position data.
+var parsedData;
+
+
+
+// Function to execute when the
+// XMLHttpRequest server (AJAX)
+// changes its ready-state.
+function ajaxCallback()
+{
+	// Check if data was received.
+	if((ajaxObj.readyState == 4) && (ajaxObj.status == 200))
+	{
+		// Parse data from JSON file.
+		parsedData = JSON.parse(ajaxObj.responseText);
+		for(i = 0; i < parsedData.length; i++)
+		{
+			// Create LatLng object for current
+			// classmate's position.
+			var currDataLoc = new google.maps.LatLng(parsedData[i].lat,parsedData[i].lng);
+			// Generate marker options for
+			// current classmate.
+			classMarkerOptions[i] = {
+					map: map,
+					title: parsedData[i].login,
+					position: currDataLoc,
+					visible: true,
+					icon: "../assets/rickles_noBack_small.png"
+				};
+			// Create a marker for the current classmate.
+			classMarkers[i] = new google.maps.Marker(classMarkerOptions[i]);
+			// Create a marker info bubble for the current classmate.
+			classInfoWindows[i] = new google.maps.InfoWindow({
+					content: classMarkers[i].getTitle()
+				});
+			// Open the info window.
+			classInfoWindows[i].open(map,classMarkers[i]);
+			// Add click event listener for info window.
+			classMarkerClickListeners = google.maps.event.addListener(classMarkers[i],"click",function(){
+					classInfoWindows[i].open(map,classMarkers[i]);
+				});
+		}
+	}
+}
+
+
 
 // This function runs when HTML body loads.
 function init()
@@ -33,24 +91,31 @@ function init()
 			// Create a marker at my current location.
 			// Add an info window to display my imaginary
 			// username.
-			var markerOptions = {
+			myMarkerOptions = {
 					map: map,
 					// My imaginary username.
-					title: "KelleyAmmerman",
+					title: myLogin,
 					position: currLoc,
 					visible: true,
 					icon: "../assets/rickles_noBack_small.png"
 				};
-			myMarker = new google.maps.Marker(markerOptions);
-			var infoWindow = new google.maps.InfoWindow({
+			myMarker = new google.maps.Marker(myMarkerOptions);
+			myInfoWindow = new google.maps.InfoWindow({
 					content: myMarker.getTitle()
 				});
 			// Open the info window.
-			infoWindow.open(map,myMarker);
+			myInfoWindow.open(map,myMarker);
 			// Add click event listener for info window.
-			google.maps.event.addListener(myMarker,"click",function(){
+			myMarkerClickListener = google.maps.event.addListener(myMarker,"click",function(){
 					infoWindow.open(map,myMarker);
 				});
+			// Load JSON data of the rest
+			// of the class, send my location
+			// and username.
+			ajaxObj.open("POST","https://secret-about-box.herokuapp.com/sendLocation",true);
+			ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			ajaxObj.onreadystatechange = ajaxCallback;
+			ajaxObj.send("login="+myLogin+"&lat="+myLat+"&lng="+myLng);
 		});
 	}
 	else
