1fbfda661861298b9780ad10b7463dc0c6171d28
Andrew_Schneer <aschneer738@gmail.com>
2015-03-12 23:34:42 -0400

    Finished updated code structure, cleaned up, reviewing requirements for last check.

--- mmap/js/mmap.js
+++ mmap/js/mmap.js
@@ -5,24 +5,55 @@ var myInfo = {
 	lng: 0,
 };
 var ajaxObj = new XMLHttpRequest();
-// Array containing initial options for the map.
-var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(myInfo.lat,myInfo.lng),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-};
 var map;
-// Object for user's position marker.
+// Arrays to store user's information.
 var myMarker;
-// Object for user's info window.
 var myInfoWindow;
-// Object for user's click event listener.
 var myMarkerClickListener;
-// Array to store the markers
-// for the class.
+// Arrays to store class's information.
 var classMarkers = [];
 var classInfoWindows = [];
 var classMarkerClickListeners = [];
+
+
+
+// Update class's marker info-windows
+// with calculated Haversine distances.
+function classInfoWindowAddContent(index,dist)
+{
+	classInfoWindows[index].setContent(
+		"<h3>"+classMarkers[index].getTitle()
+		+"</h3>"+"<p>"+dist
+		+" miles away</p>"
+	);
+}
+
+// Calculate distance between two lat/lng
+// points using Haversine formula.
+function calcHaversineDist(index)
+{
+	// Save coordinates in radians.
+	var myLat = myInfo.lat*(Math.PI/180);
+	var myLng = myInfo.lng*(Math.PI/180);
+	var theirLat = (classMarkers[index].getPosition().lat())*(Math.PI/180);
+	var theirLng = (classMarkers[index].getPosition().lng())*(Math.PI/180);
+	// Haversine calculation parameters.
+	var R = 6371000;	//meters.
+	var phi1 = myLat;
+	var phi2 = theirLat;
+	var deltaPhi = (phi2 - phi1);
+	var deltaLambda = (theirLng - myLng);
+	var a = (Math.pow((Math.sin(deltaPhi/2)),2)
+			+ (Math.cos(phi1)*Math.cos(phi2)
+			*Math.pow((Math.sin(deltaLambda/2)),2)));
+	var c = (2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a)));
+	var d = ((0.621371/1000) * (R * c));	//Convert meters to miles.
+	// Round "d" to 5 decimal places.
+	d = (Math.round(d * 100000) / 100000);
+	// Update classmate info window with
+	// appropriate content.
+	classInfoWindowAddContent(index,d);
+}
 
 // Function to generate click event
 // listener for a classmate's marker.
@@ -32,14 +63,45 @@ function addClassClickListener(index,end)
 	classMarkerClickListeners[index] = google.maps.event.addListener(classMarkers[index],"click",function(){
 			classInfoWindows[index].open(map,classMarkers[index]);
 	});
-	if(index == end)
+	// Calculate the distance of the current classmate from
+	// the user using the Haversine formula.
+	calcHaversineDist(index);
+}
+
+// Function to add classmate's marker
+// to the map at a particular
+// lat/lng location.  The
+// argument is an object with
+// fields "lat" and "lng".
+function addClassMarkers(classDataArray)
+{
+	// Loop through class data and create
+	// markers and info windows.
+	for(var i = 0; i < classDataArray.length; i++)
 	{
-		// Create Google Maps LatLng object for
-		// user's current position.
-		var currentLatLng = new google.maps.LatLng(myInfo.lat,myInfo.lng);
-		// Call function to place a marker
-		// at the user's current position.
-		addUserMarker(currentLatLng);
+		// Create LatLng object for current
+		// classmate's position.
+		var currDataLoc = new google.maps.LatLng(classDataArray[i].lat,classDataArray[i].lng);
+		// Generate marker options for
+		// current classmate.
+		var markerOptions = {
+				map: map,
+				title: classDataArray[i].login,
+				position: currDataLoc,
+				visible: true,
+				icon: "../assets/rickles_noBack_small.png"
+		};
+		// Create a marker for the current classmate.
+		classMarkers[i] = new google.maps.Marker(markerOptions);
+		// Create a marker info bubble for the current classmate.
+		classInfoWindows[i] = new google.maps.InfoWindow({
+				content: ""
+		});
+		// Open the info window by default.
+//		classInfoWindows[i].open(map,classMarkers[i]);
+		// Call function to add click event listener
+		// for each classmate's marker.
+		addClassClickListener(i,(classDataArray.length-1));
 	}
 }
 
@@ -82,7 +144,8 @@ function addUserMarker(LatLngObj)
 	// for user's marker and update
 	// global "myInfoWindow" variable.
 	myInfoWindow = new google.maps.InfoWindow({
-		content: myMarker.getTitle()
+		content: ("<h3>"+myMarker.getTitle()
+				+"</h3>")
 	});
 	// Open the user's info window
 	// by default.
@@ -93,48 +156,25 @@ function addUserMarker(LatLngObj)
 		myInfoWindow.open(map,myMarker);		
 	});
 	// Center the map on the user's marker.
+	// NOTE: This won't work if the info
+	// windows for all the markers are opened
+	// by default, as they are here.  Comment
+	// that line out in order for this to work.
 	map.setCenter(myMarker.getPosition());
-	map.panTo(map.getCenter());
-
-	// END OF FUNCTION CALL TREE //
 }
 
-// Function to add classmate's marker
-// to the map at a particular
-// lat/lng location.  The
-// argument is an object with
-// fields "lat" and "lng".
-function addClassMarkers(classDataArray)
+// Grab the class' data.
+// Call a function to add markers at
+// the class' current locations.
+function addClassToMap()
 {
-	// Loop through class data and create
-	// markers and info windows.
-	for(var i = 0; i < classDataArray.length; i++)
-	{
-		// Create LatLng object for current
-		// classmate's position.
-		var currDataLoc = new google.maps.LatLng(classDataArray[i].lat,classDataArray[i].lng);
-		// Generate marker options for
-		// current classmate.
-		var markerOptions = {
-				map: map,
-				title: classDataArray[i].login,
-				position: currDataLoc,
-				visible: true,
-				icon: "../assets/rickles_noBack_small.png"
-		};
-		// Create a marker for the current classmate.
-		classMarkers[i] = new google.maps.Marker(markerOptions);
-		// Create a marker info bubble for the current classmate.
-		classInfoWindows[i] = new google.maps.InfoWindow({
-				content: classMarkers[i].getTitle()
-				//--ADD DISTANCE FROM ME--
-		});
-		// Open the info window by default.
-		classInfoWindows[i].open(map,classMarkers[i]);
-		// Call function to add click event listener
-		// for each classmate's marker.
-		addClassClickListener(i,(classDataArray.length-1));
-	}
+	// Load JSON data of the rest
+	// of the class, send my location
+	// and username.
+	ajaxObj.open("POST","https://secret-about-box.herokuapp.com/sendLocation",true);
+	ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	ajaxObj.onreadystatechange = ajaxCallback;
+	ajaxObj.send("login="+myInfo.login+"&lat="+myInfo.lat+"&lng="+myInfo.lng);
 }
 
 // Grab the user's current GPS location.
@@ -154,6 +194,12 @@ function getUserLocation()
 			// Add class markers to
 			// the map.
 			addClassToMap();
+			// Create Google Maps LatLng object for
+			// user's current position.
+			var currentLatLng = new google.maps.LatLng(myInfo.lat,myInfo.lng);
+			// Call function to place a marker
+			// at the user's current position.
+			addUserMarker(currentLatLng);
 		});
 	}
 	else
@@ -162,23 +208,15 @@ function getUserLocation()
 	}
 }
 
-// Grab the class' data.
-// Call a function to add markers at
-// the class' current locations.
-function addClassToMap()
-{
-	// Load JSON data of the rest
-	// of the class, send my location
-	// and username.
-	ajaxObj.open("POST","https://secret-about-box.herokuapp.com/sendLocation",true);
-	ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	ajaxObj.onreadystatechange = ajaxCallback;
-	ajaxObj.send("login="+myInfo.login+"&lat="+myInfo.lat+"&lng="+myInfo.lng);
-}
-
 // This function runs when HTML body loads.
 function init()
 {
+	// Object containing initial options for the map.
+	var mapOptions = {
+			zoom: 8,
+			center: new google.maps.LatLng(myInfo.lat,myInfo.lng),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 	// Create the map object.  This is global.
 	map = new google.maps.Map(document.getElementById('map-canvas'),
 		  mapOptions);
